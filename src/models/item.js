7b89a148a108e8ecf17a65a238ca9ac5e50a9765
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-12 06:51:29 -0500

    changed monster truck image and editted text

--- src/models/item.js
+++ src/models/item.js
@@ -2,15 +2,15 @@ const item1 =
   {
     child: 'Karla',
     toy: {
-     name:  'Mario',
+     name:  ' Mario',
      weight: '1LB',
      height: '6inches',
      image:"https://media.tenor.com/77TxJYKI-WwAAAAC/mario-dance.gif",
     features: [
         'run', 'talk','dance'
       ],
-      color:  'Blue and Red',
-      cost:  `25 dollars`
+      color:  ' Blue and Red',
+      cost:  `  25 dollars`
     },
     paid: false
   }
@@ -19,15 +19,15 @@ const item2 =
   {
     child: 'Roy',
     toy: {
-     name: 'Monster Truck',
+     name: ' Monster Truck',
      weight: '3LB',
      height: '20inches',
-     image:"https://media.rainpos.com/7060/71j6AheQNL._AC_SL1500_.jpg",
+     image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1hpSO89tcW36C0TIXIRYI6APUd5pTLcH4IHfPEfnSx6qBqg-5KMrsg53zM7-i_ph1GVA&usqp=CAU",
     features: [
         'roar', 'spin','jump'
       ],
-      color: 'Red and Blue',
-      cost:  `50 Dollars`
+      color: ' Red and Light Blue',
+      cost:  ` 50 Dollars`
     },
     paid: true
   }
@@ -36,14 +36,14 @@ const item3 =
   {
    child: 'Lan',
     toy: {
-     name: 'Teddy Bear',
+     name: ' Teddy Bear',
      weight: '4LB',
      height: '8inches',
      image:"https://media.tenor.com/CSEmGqY57QAAAAAM/excited-clapping.gif",
     features: [
         'sing', 'clap','blink'
       ],
-      color: 'Brown',
+      color: ' Brown',
       cost: ` 30 Dollars`
     },
     paid: true
